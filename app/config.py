e6091fa6321c3f489c523fa3846c2d9d32deb201
edudbs <edudbs@gmail.com>
2025-11-16 13:32:10 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,10 +6,7 @@ class Settings(BaseSettings):
     youtube_api_key: str = Field(..., env="YOUTUBE_API_KEY")
     tmdb_api_key: str = Field(..., env="TMDB_API_KEY")
     database_url: str = Field("sqlite+aiosqlite:///./media_recommender.db", env="DATABASE_URL")
-    twilio_account_sid: str = Field(..., env="TWILIO_ACCOUNT_SID")
-    twilio_auth_token: str = Field(..., env="TWILIO_AUTH_TOKEN")
-    twilio_whatsapp_number: str = Field(..., env="TWILIO_WHATSAPP_NUMBER")
-
+   
     class Config:
         env_file = ".env"
         case_sensitive = True
